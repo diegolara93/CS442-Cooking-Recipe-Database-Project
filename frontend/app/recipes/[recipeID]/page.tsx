7dd756dbcedceee349891206026777d315efec12
Diego Lara <diegolara93345@gmail.com>
2025-12-12 13:09:49 -0600

    remove no-store

--- frontend/app/recipes/[recipeID]/page.tsx
+++ frontend/app/recipes/[recipeID]/page.tsx
@@ -1,11 +1,10 @@
 import { RecipeDetails } from "@/src/components/RecipeDetails";
 import type { ApiRecipe } from "@/src/types/recipe";
 
-const API_BASE = process.env.NEXT_PUBLIC_BACKEND_UR ?? "http://localhost:8080";
+const API_BASE = process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:8080";
 
 async function getRecipe(recipeID: string): Promise<ApiRecipe> {
   const res = await fetch(`${API_BASE}/api/recipes/r/byId/${recipeID}`, {
-    cache: "no-store",
   });
 
   if (!res.ok) {
